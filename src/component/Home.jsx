9d1c242af2b4321d9d79cce2a970786dc406c057
Ayoosh <>
2025-05-28 12:12:50 +0300

    first commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 
 function Home() {
+ const [character, setCharacter] = useState([]);
   const [image, setImage] = useState([]);
    const [name, setName] = useState([]);
     const [gender, setGender] = useState([]);
@@ -11,10 +12,14 @@ function Home() {
 let url = "https://6836b805664e72d28e41cf24.mockapi.io/character"
 useEffect(() => {
   axios.get(url).then((res) => {
-    setName(res.data);
+    setCharacter(res.data);
   });
   
 }, [url]);
+
+const add = () => {
+
+};
 
   return (
       
@@ -22,7 +27,7 @@ useEffect(() => {
 
     <div className=" bg-gray-200 p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto gap-5 items-center justify-center">
-      {name.map((character) => (
+      {character.map((character) => (
         <div key={character.id} className="flex flex-col bg-white p-4 mb-4 rounded shadow text-center w-[80%] mx-auto">
             <img src={character.image} alt="image" />
               <h2 className="text-lg text-black font-bold">{character.name}</h2>
